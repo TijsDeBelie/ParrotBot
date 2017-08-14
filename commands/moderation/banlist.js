ba01041b8dd3cdb0e50f5f07e230599027f0be99
TijsDeBelie <tijs.debelie@student.odisee.be>
2017-08-15 00:21:10 +0200

    banlist edit 2

--- commands/moderation/banlist.js
+++ commands/moderation/banlist.js
@@ -1,9 +1,14 @@
 const { Command } = require('discord.js-commando');
+var automod = require(appRoot +'/automod.js')
+module.exports = class Banlist extends Command {
     constructor(client) {
         super(client, {
             name: 'banlist',
+            aliases: ['log'],
             group: 'moderation',
             memberName: 'banlist',
+            description: 'Gives a list of recent warned and banned users',
+            examples: ['$banlist'],
             throttling: {
                 usages: 1,
                 duration: 10
@@ -11,7 +16,9 @@ const { Command } = require('discord.js-commando');
         });
     }
 
+    run(msg) {
 
+       msg.channel.send("Warned users: " + warned + "\nBanned users: " + banlist);
 
 
     }
