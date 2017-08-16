274b94c1719f3cdaa8669fa1718e42bc45c006e1
TijsDeBelie <tijs.debelie@student.odisee.be>
2017-08-16 14:54:43 +0200

    added info command

---
+++ commands/moderation/info.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class Addrole extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'info',
+            group: 'moderation',
+            memberName: 'info',
+            description: 'displays how many guilds the bot is in',
+            examples: ['$info'],
+            throttling: {
+                usages: 1,
+                duration: 10
+            },
+            
+        });
+    }
+
+    run(msg, args) {
+       
+            if (!msg.member.hasPermission('ADMINISTRATOR')){
+                return msg.say('Error! You don\'t have permission to grant ' + rank + ' to ' + user + "!");}
+            else {
+
+                msg.channel.send(client.guilds.size.toLocaleString() + " " + client.channels.array());
+           
+            }
+        }
+};
